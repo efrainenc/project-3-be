434cdba9115d76a0ab975b515098e256c3199d1a
unknown <efraine387@gmail.com>
2023-01-04 10:39:13 -0500

    Mongo uri setup

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const morgan = require('morgan')
 // import express
 const express = require("express");
 
-const peopleController = require('./controllers/home-controller')
+const homeController = require('./controllers/home-controller')
 
 // create application object
 const app = express();
@@ -25,35 +25,34 @@ const app = express();
 app.use(cors()) // allows for cross origin request - open channel
 app.use(morgan('dev')) // morgan request logger (for dev)
 app.use(express.json()) // allows us to parse json data
-app.use('/people', peopleController)
+app.use('/home', homeController)
 
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
-app.get('/', (req, res)=>res.redirect('/people'))
+app.get('/', (req, res)=>res.redirect('/home'))
 
-// // Basic error handling for bad product indexes
-// app.get('/error', (req,res)=>{
-//   res.status(500).send('something went wrong...')
-// })
+// Error handling
+app.get('/error', (req,res)=>{
+  res.status(500).send('something went wrong...')
+})
 
-// // Error handling using the next argument + middleware
-// app.use((error, req,res,next)=>{
-//   if(error){
-//       return res.status(404).send(error.message)
-//   }
-//   next()
-// })
+app.use((error, req,res,next)=>{
+  if(error){
+      return res.status(404).send(error.message)
+  }
+  next()
+})
 
-// // wild card / 404 if not using error handling middleware 
-// app.get('*', (req,res,next)=>{
-//   if(req.error){
-//       res.status(404).send(`Error: ${req.error.message}`)
-//   }else {
-//       res.redirect('/error/')
-//   }
-// })
+// wild card
+app.get('*', (req,res,next)=>{
+  if(req.error){
+      res.status(404).send(`Error: ${req.error.message}`)
+  }else {
+      res.redirect('/error/')
+  }
+})
 
 ///////////////////////////////
 // LISTENER
