734e0047789ea43e7f2b3ace8a932e9b8d0447f4
unknown <efraine387@gmail.com>
2023-01-07 18:26:56 -0500

    dev

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,7 +25,6 @@ router.get('/', async (req, res) => {
 	}
 });
 
-
 // show route (GET HTTP VERB)
 // this route will catch GET requests to /products/index/ and respond with a single product
 router.get('/:id', async (req, res, next) => { 
@@ -41,48 +40,20 @@ try {
 }
 });
 
-// create route (POST HTTP VERB)
-// send data to create a new product
-// COMMENT 
-router.post("/", requireToken, async (req, res, next) => {
-  try {
-		// passport will verify the the token passed with the request's Authorization headers and set the current user for the request (req.user).
-		// const owner = req.user._id
-		// req.body.owner = owner
-    const newPost = await db.User.create(req.body);
-    res.status(201).json(newPost);
-  } catch (err) {
-    res.status(400).json({
-      error: err.message,
-    });
-  }
-});
-
 // update route (PUT HTTP VERB)
 router.put("/:id", requireToken, async (req, res) => {
 	try {
 		handleValidateOwnership(req, await db.User.findById(req.params.id))
-		const updatedPost = await db.User.findByIdAndUpdate(
+		const updatedUser = await db.User.findByIdAndUpdate(
 			req.params.id,
 			req.body,
 			{ new: true }
 		)
-		res.status(200).json(updatedPost)
+		res.status(200).json(updatedUser)
 	} catch (error) {
 		//send error
 		res.status(400).json({error: error.message})
 	}
 })
-
-// destroy route (DELETE HTTP VERB)
-router.delete("/:id", requireToken, async (req, res, next) => {
-  try {
-    handleValidateOwnership(req, await db.User.findById(req.params.id));
-    const deletedPost = await db.User.findByIdAndRemove(req.params.id);
-    res.status(200).json(deletedPost);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
 
 module.exports = router
